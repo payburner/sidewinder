792732b047cf0f84d5291d692236742ad0c1f0c7
Chris Opler <chrisopler@yahoo.com>
2021-05-13 05:38:27 -0400

    Update constraints

--- sidewinder-trading/camunda-process-server/src/main/java/com/github/huksley/camunda/UniqueKeys.java
+++ sidewinder-trading/camunda-process-server/src/main/java/com/github/huksley/camunda/UniqueKeys.java
@@ -8,14 +8,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UniqueKeys {
+
   @Autowired
   private JdbcTemplate jdbcTemplate;
+
   @EventListener(ApplicationReadyEvent.class)
   public void doSomethingAfterStartup() {
-    jdbcTemplate.execute(
-        "alter table ACT_RU_EXECUTION add constraint if not exists ACT_UNIQ_RU_BUS_KEY UNIQUE (PROC_DEF_ID_, BUSINESS_KEY_)");
-    jdbcTemplate.execute(
-        "alter table ACT_HI_PROCINST add constraint if not exists ACT_UNIQ_HI_BUS_KEY UNIQUE (PROC_DEF_ID_, BUSINESS_KEY_)");
+    try {
+      jdbcTemplate.execute(
+          "alter table ACT_RU_EXECUTION add constraint ACT_UNIQ_RU_BUS_KEY UNIQUE (PROC_DEF_ID_, BUSINESS_KEY_)");
+    } catch (Exception e) {
 
+    }
+    try {
+      jdbcTemplate.execute(
+          "alter table ACT_HI_PROCINST add constraint ACT_UNIQ_HI_BUS_KEY UNIQUE (PROC_DEF_ID_, BUSINESS_KEY_)");
+    } catch (Exception e) {
+
+    }
   }
 }
