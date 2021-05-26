d1ecef34e0be10a3e9761a9ea7cc518ee166c8b8
Chris Opler <chrisopler@yahoo.com>
2021-05-25 20:05:34 -0400

    add front-end

---
+++ sidewinder-trading/sidewinder-trading-api-service/index.js
@@ -0,0 +1,47 @@
+const AWS = require('aws-sdk');
+const {SidewinderOmsPersistenceService} = require("@payburner/sidewinder-trading-oms-p/src/SidewinderOmsPersistenceService");
+const fs = require('fs');
+const uuid4 = require('uuid4');
+const express = require('express');
+const http = require('http');
+const axios = require('axios');
+const app = express();
+
+const pConfig = JSON.parse(fs.readFileSync(process.argv[2]).toString());
+
+const basicAuthentication = new BasicAuthInterceptor({
+    username: pConfig.CAMUNDA_USER,
+    password: pConfig.CAMUNDA_PASSWORD
+});
+
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { interceptors: basicAuthentication, baseUrl: "https://oms.payburner.com/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+AWS.config.update({
+    accessKeyId: pConfig.AWS_ACCESS_ID,
+    secretAccessKey: pConfig.AWS_ACCESS_KEY,
+    region: pConfig.AWS_REGION
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const p = new SidewinderOmsPersistenceService( docClient );
+
+app.get('/', function(req, res, next) {
+    res.status(200).send({data:1});
+});
+
+app.use(express.static('public'));
+app.use(function (req, res, next) {
+    res.status(404).send({ status: 404, error: 'The path was not found.'})
+})
+http.createServer(app).listen(3553, function() {
+    console.log('Server HTTP server listening on port ' + 3553);
+
+});
+
---
+++ sidewinder-trading/sidewinder-trading-api-service/install.sh
@@ -0,0 +1,3 @@
+sudo mkdir /etc/sidewinder-trading/sidewinder-trading-api-service
+sudo mkdir /etc/sidewinder-trading/sidewinder-trading-api-service/logs
+sudo forever start ./forever/prod.json
