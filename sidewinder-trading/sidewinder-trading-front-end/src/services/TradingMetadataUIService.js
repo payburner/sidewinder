130df33e4bb313cabe9fe4282e2d802f3c3264e0
Chris Opler <chrisopler@yahoo.com>
2021-05-27 06:05:07 -0400

    update ui

--- sidewinder-trading/sidewinder-trading-front-end/src/services/TradingMetadataUIService.js
+++ sidewinder-trading/sidewinder-trading-front-end/src/services/TradingMetadataUIService.js
@@ -1,13 +1,20 @@
 export default class TradingMetadataUIService {
 
+
     constructor( tradingMetadataService ) {
         this.tradingMetadataService = tradingMetadataService;
+        this.noSymbols = [
+            'USDC','GUSD','PAX','DAI','MKR','LINK'
+        ]
     }
 
     assetIconClass = function ( asset, size ) {
         const assetType = this.tradingMetadataService.assetType( asset );
         if (assetType === 'FIAT') {
            return 'fiat-icon-' + size + ' fa fa-' + asset.toLowerCase();
+        }
+        else if (this.noSymbols.indexOf(asset) > -1) {
+            return 'fiat-icon-' + size + ' fa fa-coin';
         }
         else {
             return 'cc ' + asset;
