382c26360a19c5fe188ba0ce3640bc4af52d49d5
Chris Opler <chrisopler@yahoo.com>
2021-05-28 15:04:06 -0400

    UI Update

--- sidewinder-trading/sidewinder-trading-front-end/src/pages/Login.js
+++ sidewinder-trading/sidewinder-trading-front-end/src/pages/Login.js
@@ -15,7 +15,7 @@ export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.notifierService = new NotifierService();
-        this.state = { token}
+     
     }
 
     render() {
