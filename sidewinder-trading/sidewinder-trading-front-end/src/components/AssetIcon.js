44a9786862aa8d3a8dd38f5191606e7bc850b655
Chris Opler <chrisopler@yahoo.com>
2021-05-29 03:26:02 -0400

    UI Update

--- sidewinder-trading/sidewinder-trading-front-end/src/components/AssetIcon.js
+++ sidewinder-trading/sidewinder-trading-front-end/src/components/AssetIcon.js
@@ -20,6 +20,9 @@ export default class AssetIcon extends React.Component {
 
     render() {
         const asset = this.props.asset;
+        if (asset === 'null') {
+            alert('asset is null');
+        }
         const assetType = this.assetType( asset );
         const classes = typeof this.props.classes !== 'undefined' ? ' ' + this.props.classes : ' ';
         const size = typeof this.props.size !== 'undefined' ? this.props.size : 'medium';
@@ -38,7 +41,7 @@ export default class AssetIcon extends React.Component {
             else if (size === 'small') {
                 height = '22px';
             }
-            return <img style={style} height={height} src={'/resources/icons/128/color/' + asset + '.png'} className={classes}/>
+            return <img style={style} height={height} src={'/resources/icons/128/color/' + asset.toLowerCase() + '.png'} className={classes}/>
         }
     }
 }
